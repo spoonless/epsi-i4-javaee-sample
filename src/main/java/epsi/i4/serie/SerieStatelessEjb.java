446bdd47e338412251496ec7bdf1afc96a05a1ae
David Gayerie <dagaydevel@free.fr>
2014-01-18 02:49:08 +0100

    ajout d'un implémentation naive pour le gestion des transactions par le bean

--- src/main/java/epsi/i4/serie/SerieStatelessEjb.java
+++ src/main/java/epsi/i4/serie/SerieStatelessEjb.java
@@ -1,5 +1,6 @@
 package epsi.i4.serie;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
@@ -7,27 +8,37 @@ import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 @Stateless
-@TransactionManagement(TransactionManagementType.CONTAINER)
+@TransactionManagement(TransactionManagementType.BEAN)
 @TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class SerieStatelessEjb {
 	
 	@PersistenceContext(unitName="serie-persistence-unit")
 	private EntityManager entityManager;
 	
-	public void create(Serie serie) {
+	@Resource
+	private UserTransaction userTransaction;
+	
+	public void create(Serie serie) throws Exception {
+		userTransaction.begin();
 		entityManager.persist(serie);
+		userTransaction.commit();
 	}
 	
-	public void update(Serie serie) {
+	public void update(Serie serie) throws Exception {
+		userTransaction.begin();
 		entityManager.merge(serie);
+		userTransaction.commit();
 	}
 
-	public void delete(Serie serie) {
+	public void delete(Serie serie) throws Exception {
+		userTransaction.begin();
 		entityManager.createQuery("delete from Serie s where s.id = :id")
 		             .setParameter("id", serie.getId())
 		             .executeUpdate();
+		userTransaction.commit();
 	}
 	
 	public Serie get(long id) {
